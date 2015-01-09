84e94eddc3c36bb7f60be76b1539baf22b616bc2
jensfinnas <jens.finnas@gmail.com>
2015-01-09 14:40:45 +0100

    Subtitles for all groups

--- app/scripts/subtitles.js
+++ app/scripts/subtitles.js
@@ -1,4 +1,14 @@
-
+var numberToText = {
+	1: 'en',
+	2: 'två',
+	3: 'tre',
+	4: 'fyra',
+	5: 'fem',
+	6: 'sex',
+	7: 'sju',
+	8: 'åtta',
+	9: 'nio',
+};
 // Generate dynamic subtitles for specfic categories
 var dynamicSubtitles = {
 	'Utbildning': function(values) {
@@ -20,11 +30,11 @@ var dynamicSubtitles = {
 		var strToday = 'Arbetslösheten är just nu högst bland akademiker utbildade inom ' + highest.nameFull.toLowerCase() + '. De har ' + diff + ' procentenheter högre arbetslöshet än personer inom ' + lowest.nameFull.toLowerCase() + '.<br/> ' ;	
 		var strChange = 'Jämfört med samma tidpunkt förra året ';
 		if (increaseShare > decreaseShare) {
-			var shareStr = increaseShare == 1 ? 'samtliga' : increaseN + ' av ' + totalN; 
+			var shareStr = increaseShare == 1 ? 'samtliga' : numberToText[increaseN] + ' av ' + numberToText[totalN]; 
 			strChange += 'ökar inom ' + shareStr + ' utbildningsgrupper.' 
 		}
 		else {
-			var shareStr = decreaseShare == 0 ? 'samtliga' : decreaseN + ' av ' + totalN; 
+			var shareStr = decreaseShare == 0 ? 'samtliga' : numberToText[decreaseN] + ' av ' + numberToText[totalN]; 
 			strChange += 'minskar arbetslöshet inom ' + shareStr + ' utbildningsgrupper.';	
 		}
 		return strToday + ' ' + strChange;
@@ -61,11 +71,53 @@ var dynamicSubtitles = {
 
 		return str;	
 	},
-	'Ålder': function() {
-		return 'Här kommer en autogeneread text som beskriver vad grafen visar. Typ säga vilka som ökat mest.'	
+	'Ålder': function(values) {
+		var valuesSortedToday = values.slice(0).sort(function(a, b){ 
+				return d3.descending(a.today, b.today);
+			});
+		var valuesSortedChange = values.slice(0).sort(function(a, b){ 
+				return d3.descending(a.change, b.change);
+			});
+
+		var highest = valuesSortedToday[0];
+		var lowest = valuesSortedToday[valuesSortedToday.length - 1];
+		var diff = formatPercent(highest.today - lowest.today).replace("%","");
+		var totalN = values.length;
+		var increaseN = values.filter(function(d) { return d.change > 0 }).length;
+		var decreaseN = values.filter(function(d) { return d.change < 0 }).length;
+		var increaseShare = increaseN / totalN;
+		var decreaseShare = decreaseShare / totalN;
+		var strToday = 'Arbetslösheten är just nu högst bland akademiker i åldern ' + highest.nameFull.toLowerCase() + '. De har ' + diff + ' procentenheter högre arbetslöshet än personer i åldern ' + lowest.nameFull.toLowerCase() + '.<br/> ' ;	
+		var strChange = 'Jämfört med samma tidpunkt förra året ';
+		if (increaseShare > decreaseShare) {
+			var shareStr = increaseShare == 1 ? 'samtliga' : numberToText[increaseN] + ' av ' + numberToText[totalN]; 
+			strChange += 'ökar inom ' + shareStr + ' utbildningsgrupper.' 
+		}
+		else {
+			var shareStr = decreaseShare == 0 ? 'samtliga' : numberToText[decreaseN] + ' av ' + numberToText[totalN]; 
+			strChange += 'minskar arbetslöshet inom ' + shareStr + ' åldersgrupper.';	
+		}
+		return strToday + ' ' + strChange;
 	},
-	'Födelseplats': function() {
-		return 'Här kommer en autogeneread text som beskriver vad grafen visar. Typ säga vilka som ökat mest.'	
+	'Födelseplats': function(values) {
+		var swedenBorn = values.filter(function(d) {
+			return d.name == 'Sverige' 
+		})[0];
+		var foreignBorn = values.filter(function(d) {
+			return d.name == 'Utanför Norden' 
+		})[0];
+
+		var diffToday = formatPercent(foreignBorn.today - swedenBorn.today).replace('%','');
+		var diffChange = foreignBorn.change - swedenBorn.change;
+		var diffChangeStr = formatPercent(diffChange).replace('%','');
+		var direction = foreignBorn.change > swedenBorn.change ? 'dessutom ökat' : 'dock minskat';
+
+		var str = 'Var en person är född är en av de mest avgörande faktorerna för risken att bli arbetslös som akademiker. Personer födda utanför Norden har ' + diffToday + ' procentenheter högre arbetslöshet än svenskfödda. ';
+		
+		if (diffChange!==0) {
+			str += ' Skillnaden har ' + direction + ' något (' + diffChangeStr + ' procentenheter) sedan förra året.';
+		}
+		return str;
 	}
 
 }
